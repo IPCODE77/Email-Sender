ce1afe476ac6b2dc1d9eb0532f266ba6a51c5339
ITISH KUMAR PATI <89587666+IPCODE77@users.noreply.github.com>
2023-08-08 11:40:15 +0530

    Update settings.py
    Remove the  smtp login of mail

--- emailsender/settings.py
+++ emailsender/settings.py
@@ -116,8 +116,8 @@ USE_TZ = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "kumaritish00@gmail.com"
-EMAIL_HOST_PASSWORD = "xxvprhkwwtyidmdv"
+EMAIL_HOST_USER = "Yourmail@gmail.com"
+EMAIL_HOST_PASSWORD = "yourapppassword"
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
